31237730d950ae9545efdd471a83ce57fd530449
Ryan Beisner <ryan.beisner@canonical.com>
2016-09-29 00:48:28 +0000

    Re-tag the woops

--- tools/deploy-on-openstack.sh
+++ tools/deploy-on-openstack.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+tmp_bundle=$(mktemp)
+echo $tmp_bundle
+
 # Untag machines for use with the Juju OpenStack provider
-sed -e "s# tags=demo##g" juju-bundles/spark-hadoop-processing.yaml -i
+sed -e "s# tags=demo##g" juju-bundles/spark-hadoop-processing.yaml > $tmp_bundle
 
 # Deploy
-juju-deployer -vdc juju-bundles/spark-hadoop-processing.yaml
+juju-deployer -vdc $tmp_bundle

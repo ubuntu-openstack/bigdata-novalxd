111d5b5acf26ce38ad94135bff86d4a931788de8
Ryan Beisner <ryan.beisner@canonical.com>
2016-09-29 17:38:21 +0000

    Update deploy scripts

--- tools/deploy-spark-on-metal.sh
+++ tools/deploy-spark-on-metal.sh
@@ -2,5 +2,6 @@
 # Build venv with juju-deployer from trunk, deploy
 tox
 . .tox/deployer/bin/activate
-juju bootstrap --constraints "arch=amd64 tags=demo"
+juju bootstrap --constraints "arch=amd64 tags=demo" ||:
 juju-deployer -vdc juju-bundles/spark-hadoop-processing.yaml
+host $(juju-deployer -f resourcemanager)
--- tools/deploy-spark-on-openstack.sh
+++ tools/deploy-spark-on-openstack.sh
@@ -4,5 +4,6 @@ tmp_bundle=$(mktemp)
 sed -e "s# tags=demo##g" juju-bundles/spark-hadoop-processing.yaml > $tmp_bundle
 tox
 . .tox/deployer/bin/activate
+juju bootstrap ||:
 juju-deployer -vdc $tmp_bundle
 tools/float-namenode.sh

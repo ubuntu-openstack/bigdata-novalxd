77791d9d3e16971a4026df5bbad73073dee08970
Andrew McLeod <admcleod@gmail.com>
2016-10-14 11:23:10 +0200

    fix typos

--- tools/run_all_tests.sh
+++ tools/run_all_tests.sh
@@ -30,23 +30,23 @@ jdo resourcemanager/0 testdfsio > ../results/testdfsio_results-${etype}.txt
 # Spark tests
 echo Running spark tests...
 echo spark pi
-jdo spark/0 sparkpi ../results/sparkpi_results-${type}.txt
+jdo spark/0 sparkpi > ../results/sparkpi_results-${type}.txt
 echo spark sql
-jdo spark/0 sql ../results/sql_results-${type}.txt
+jdo spark/0 sql > ../results/sql_results-${type}.txt
 echo streaming
-jdo spark/0 streaming ../results/streaming_results-${type}.txt
+jdo spark/0 streaming > ../results/streaming_results-${type}.txt
 echo logisticregression
-jdo spark/0 logisticregression ../results/logisticregression_results-${type}.txt
+jdo spark/0 logisticregression > ../results/logisticregression_results-${type}.txt
 echo matrix factorization
-jdo spark/0 matrixfactorization ../results/matrixfactorization_results-${type}.txt
+jdo spark/0 matrixfactorization > ../results/matrixfactorization_results-${type}.txt
 echo pagerank
-jdo spark/0 pagerank ../results/pagerank_results-${type}.txt
+jdo spark/0 pagerank > ../results/pagerank_results-${type}.txt
 echo svdplusplus
-jdo spark/0 svdplusplus ../results/svdplusplus_results-${type}.txt
+jdo spark/0 svdplusplus > ../results/svdplusplus_results-${type}.txt
 echo svm
-jdo spark/0 svm ../results/svm_results-${type}.txt
+jdo spark/0 svm > ../results/svm_results-${type}.txt
 echo trianglecount
-jdo spark/0 trianglecount ../results/trianglecount_results-${type}.txt
+jdo spark/0 trianglecount > ../results/trianglecount_results-${type}.txt
 
 
 # prepare anomaly detection

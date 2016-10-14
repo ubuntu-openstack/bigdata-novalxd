6b410b00ab72772ca1049ed294344c97d0049956
Andrew McLeod <admcleod@gmail.com>
2016-10-14 09:22:34 +0200

    script fix

--- tools/run_all_tests.sh
+++ tools/run_all_tests.sh
@@ -18,19 +18,19 @@ jdo() { juju action fetch --wait 0 $(juju action do $1 $2 | awk '{print $5}'); }
 
 # Resourcemanager (yarn / hdfs) tests
 echo Running resourcemanager tests...
-jdo resourcemanager/0 mrbench > ~/mrbench_results-${etype}.txt
-jdo resourcemanager/0 nnbench > ~/nnbench_results-${etype}.txt
-jdo resourcemanager/0 terasort > ~/terasort_results-${etype}.txt
-jdo resourcemanager/0 testdfsio > ~/testdfsiio_results-${etype}.txt
+jdo resourcemanager/0 mrbench > ../results/mrbench_results-${etype}.txt
+jdo resourcemanager/0 nnbench > ../results/nnbench_results-${etype}.txt
+jdo resourcemanager/0 terasort > ../results/terasort_results-${etype}.txt
+jdo resourcemanager/0 testdfsio > ../results/testdfsio_results-${etype}.txt
 
 # Spark tests
 echo Running spark tests...
-jdo spark/0 sparkpi ~/sparkpi_results-${type}.txt
-jdo spark/0 sql ~/sql_results-${type}.txt
-jdo spark/0 streaming ~/streaming_results-${type}.txt
-jdo spark/0 logisticregression ~/logisticregression_results-${type}.txt
-jdo spark/0 matrixfactorization ~/matrixfactorization_results-${type}.txt
-jdo spark/0 pagerank ~/pagerank_results-${type}.txt
-jdo spark/0 svdplusplus ~/svdplusplus_results-${type}.txt
-jdo spark/0 svm ~/svm_results-${type}.txt
-jdo spark/0 trianglecount ~/trianglecount_results-${type}.txt
+jdo spark/0 sparkpi ../results/sparkpi_results-${type}.txt
+jdo spark/0 sql ../results/sql_results-${type}.txt
+jdo spark/0 streaming ../results/streaming_results-${type}.txt
+jdo spark/0 logisticregression ../results/logisticregression_results-${type}.txt
+jdo spark/0 matrixfactorization ../results/matrixfactorization_results-${type}.txt
+jdo spark/0 pagerank ../results/pagerank_results-${type}.txt
+jdo spark/0 svdplusplus ../results/svdplusplus_results-${type}.txt
+jdo spark/0 svm ../results/svm_results-${type}.txt
+jdo spark/0 trianglecount ../results/trianglecount_results-${type}.txt

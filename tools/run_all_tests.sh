682b8201bdbdc5f6182fbf97944e2c80bfbfdaa8
Andrew McLeod <admcleod@gmail.com>
2016-10-14 09:24:17 +0200

    logging

--- tools/run_all_tests.sh
+++ tools/run_all_tests.sh
@@ -18,19 +18,32 @@ jdo() { juju action fetch --wait 0 $(juju action do $1 $2 | awk '{print $5}'); }
 
 # Resourcemanager (yarn / hdfs) tests
 echo Running resourcemanager tests...
+echo mapreduce benchmark
 jdo resourcemanager/0 mrbench > ../results/mrbench_results-${etype}.txt
+echo namenode benchmark
 jdo resourcemanager/0 nnbench > ../results/nnbench_results-${etype}.txt
+echo terasort
 jdo resourcemanager/0 terasort > ../results/terasort_results-${etype}.txt
+echo dfsio benchmark
 jdo resourcemanager/0 testdfsio > ../results/testdfsio_results-${etype}.txt
 
 # Spark tests
 echo Running spark tests...
+echo spark pi
 jdo spark/0 sparkpi ../results/sparkpi_results-${type}.txt
+echo spark sql
 jdo spark/0 sql ../results/sql_results-${type}.txt
+echo streaming
 jdo spark/0 streaming ../results/streaming_results-${type}.txt
+echo logisticregression
 jdo spark/0 logisticregression ../results/logisticregression_results-${type}.txt
+echo matrix factorization
 jdo spark/0 matrixfactorization ../results/matrixfactorization_results-${type}.txt
+echo pagerank
 jdo spark/0 pagerank ../results/pagerank_results-${type}.txt
+echo svdplusplus
 jdo spark/0 svdplusplus ../results/svdplusplus_results-${type}.txt
+echo svm
 jdo spark/0 svm ../results/svm_results-${type}.txt
+echo trianglecount
 jdo spark/0 trianglecount ../results/trianglecount_results-${type}.txt

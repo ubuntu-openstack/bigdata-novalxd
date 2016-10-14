63b6ea091bcbd68f33e2ff0eb5105d28c6a2e8f3
Andrew McLeod <admcleod@gmail.com>
2016-10-14 13:11:10 +0200

    fix typo

--- tools/prepare_anomaly_detection.sh
+++ tools/prepare_anomaly_detection.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Get training data and cross_val to /tmp...
-juju run --unit slave/0 "wget https://raw.githubusercontent.com/mvogiatzis/spark-anomaly-detection/master/src/test/resources/training.csv -o /tmp/training.csv"
-juju run --unit slave/0 "wget https://raw.githubusercontent.com/mvogiatzis/spark-anomaly-detection/master/src/test/resources/cross_val.csv -o /tmp/cross_val.csv"
+juju run --unit slave/0 "wget https://raw.githubusercontent.com/mvogiatzis/spark-anomaly-detection/master/src/test/resources/training.csv -O /tmp/training.csv"
+juju run --unit slave/0 "wget https://raw.githubusercontent.com/mvogiatzis/spark-anomaly-detection/master/src/test/resources/cross_val.csv -O /tmp/cross_val.csv"
 echo move training data to hdfs://spark-test/resources/
 juju run --unit slave/0 "su hdfs -c 'hadoop fs -mkdir -p /spark-test/resources/'"
 juju run --unit slave/0 "su hdfs -c 'hdfs dfs -put /tmp/training.csv /spark-test/resources'"

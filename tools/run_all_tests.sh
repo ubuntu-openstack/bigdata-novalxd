87085be0a6ecff9995352cba22b7945b165f79d8
Andrew McLeod <admcleod@gmail.com>
2016-10-16 16:59:27 +0200

    add for loop

--- tools/run_all_tests.sh
+++ tools/run_all_tests.sh
@@ -14,49 +14,59 @@ juju set spark spark_execution_mode=yarn-client
 
 # Define the jdo alias for easier action submission
 
+mkdir ../results/${etype}/
+base_output_dir="../results/${etype}"
+
 jdo() { juju action fetch --wait 0 $(juju action do $1 $2 | awk '{print $5}'); }
 
-# Resourcemanager (yarn / hdfs) tests
-echo Running resourcemanager tests...
-echo mapreduce benchmark
-jdo resourcemanager/0 mrbench maps=100 reduces=50 > ../results/mrbench_results-${etype}.txt
-echo namenode benchmark
-jdo resourcemanager/0 nnbench maps=100 reduces=50 > ../results/nnbench_results-${etype}.txt
-echo terasort
-jdo resourcemanager/0 terasort maps=100 reduces=50 > ../results/terasort_results-${etype}.txt
-echo dfsio write benchmark
-jdo resourcemanager/0 testdfsio mode='write' resfile='/tmp/dfsio_write_results.txt' > ../results/testdfsio_write_results-${etype}.txt
-echo dfsio read benchmark
-jdo resourcemanager/0 testdfsio mode='read' resfile='/tmp/dfsio_read_results.txt' > ../results/testdfsio_read_results-${etype}.txt
-echo getting dfsio write and read job results
-juju scp resourcemanager/0:/tmp/dfsio_write_results.txt ../results/testdfsio_write_results_detailed-${etype}.txt
-juju scp resourcemanager/0:/tmp/dfsio_read_results.txt ../results/testdfsio_read_results_detailed-${etype}.txt
+for run_number in {1..10} ; do
 
-# Spark tests
-echo Running spark tests...
-echo spark pi
-jdo spark/0 sparkpi > ../results/sparkpi_results-${etype}.txt
-echo spark sql
-jdo spark/0 sql > ../results/sql_results-${etype}.txt
-echo streaming
-jdo spark/0 streaming > ../results/streaming_results-${etype}.txt
-echo logisticregression
-jdo spark/0 logisticregression > ../results/logisticregression_results-${etype}.txt
-echo matrix factorization
-jdo spark/0 matrixfactorization > ../results/matrixfactorization_results-${etype}.txt
-echo pagerank
-jdo spark/0 pagerank > ../results/pagerank_results-${etype}.txt
-echo svdplusplus
-jdo spark/0 svdplusplus > ../results/svdplusplus_results-${etype}.txt
-echo svm
-jdo spark/0 svm > ../results/svm_results-${etype}.txt
-echo trianglecount
-jdo spark/0 trianglecount > ../results/trianglecount_results-${etype}.txt
+       
+        # Resourcemanager (yarn / hdfs) tests
+        echo Running resourcemanager tests, run ${run_number} of 10...
+        echo mapreduce benchmark, run ${run_number}
+        jdo resourcemanager/0 mrbench maps=100 reduces=50 > ${base_output_dir}/mrbench_results-${run_number}.txt
+        echo namenode benchmark, run ${run_number}
+        jdo resourcemanager/0 nnbench maps=100 reduces=50 > ${base_output_dir}/nnbench_results-${run_number}.txt
+        echo terasort, run ${run_number}
+        jdo resourcemanager/0 terasort maps=100 reduces=50 > ${base_output_dir}/terasort_results-${run_number}.txt
+        echo dfsio write benchmark, run ${run_number}
+        jdo resourcemanager/0 testdfsio mode='write' resfile='/tmp/dfsio_write_results.txt' > ${base_output_dir}/testdfsio_write_results-${run_number}.txt
+        echo dfsio read benchmark, run ${run_number}
+        jdo resourcemanager/0 testdfsio mode='read' resfile='/tmp/dfsio_read_results.txt' > ${base_output_dir}/testdfsio_read_results-${run_number}.txt
+        echo getting dfsio write and read job results, run ${run_number}
+        juju scp resourcemanager/0:/tmp/dfsio_write_results.txt ${base_output_dir}/testdfsio_write_results_detailed-${run_number}.txt
+        juju scp resourcemanager/0:/tmp/dfsio_read_results.txt ${base_output_dir}/testdfsio_read_results_detailed-${run_number}.txt
+        
+        # Spark tests
+        echo Running spark tests..., run ${run_number}
+        echo spark pi, run ${run_number}
+        jdo spark/0 sparkpi > ${base_output_dir}/sparkpi_results-${run_number}.txt
+        echo spark sql, run ${run_number}
+        jdo spark/0 sql > ${base_output_dir}/sql_results-${run_number}.txt
+        echo streaming, run ${run_number}
+        jdo spark/0 streaming > ${base_output_dir}/streaming_results-${run_number}.txt
+        echo logisticregression, run ${run_number}
+        jdo spark/0 logisticregression > ${base_output_dir}/logisticregression_results-${run_number}.txt
+        echo matrix factorization, run ${run_number}
+        jdo spark/0 matrixfactorization > ${base_output_dir}/matrixfactorization_results-${run_number}.txt
+        echo pagerank, run ${run_number}
+        jdo spark/0 pagerank > ${base_output_dir}/pagerank_results-${run_number}.txt
+        echo svdplusplus, run ${run_number}
+        jdo spark/0 svdplusplus > ${base_output_dir}/svdplusplus_results-${run_number}.txt
+        echo svm, run ${run_number}
+        jdo spark/0 svm > ${base_output_dir}/svm_results-${run_number}.txt
+        echo trianglecount, run ${run_number}
+        jdo spark/0 trianglecount > ${base_output_dir}/trianglecount_results-${run_number}.txt
+        
+        
+        # prepare anomaly detection
+        echo Preparing anomaly detection job, run ${run_number} 
+        ./prepare_anomaly_detection.sh
+        
+        # run anomyaly detection
+        echo Anomaly detection, run ${run_number}
+        echo Check ../results/ for new files after this detection job finishes...
+        ./run_anomaly_detection.sh ${etype} ${run_number}
 
-
-# prepare anomaly detection
-./prepare_anomaly_detection.sh
-
-# run anomyaly detection
-echo Check ../results/ for new files after this detection job finishes...
-./run_anomaly_detection.sh ${etype}
+done
--- tools/run_anomaly_detection.sh
+++ tools/run_anomaly_detection.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 stackname=$1
+run_number=$2
 echo scp anomaly detection jar to spark/0:/tmp/anomaly-detection_2.10-1.0.jar
 juju scp ./bin/anomaly-detection_2.10-1.0.jar spark/0:/tmp
 echo Submit job
-juju run --unit spark/0 "rm /tmp/spark_job_results.txt"
+#juju run --unit spark/0 "rm /tmp/spark_job_results.txt"
 juju run --timeout=120m30s --unit spark/0 "su hdfs -c 'date > /tmp/spark_job_results ; time /usr/bin/spark-submit --class MainRun /tmp/anomaly-detection_2.10-1.0.jar --master yarn >> /tmp/spark_job_results.txt ; date >> /tmp/spark_job_results.txt'"
 echo get results
-juju scp spark/0:/tmp/spark_job_results.txt ../results/spark_job_results-${stackname}-$(date +%H.%M-%d%m%y)
+juju scp spark/0:/tmp/spark_job_results.txt ../results/${stackname}/spark_job_results-${run_number}.txt

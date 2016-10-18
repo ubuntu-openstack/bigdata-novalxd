f704588a0fe4b206683013c017da5ae6f57aac16
Ryan Beisner <ryan.beisner@canonical.com>
2016-10-18 18:03:55 +0000

    Update instance launch script

--- tools/instance-launch.sh
+++ tools/instance-launch.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -e
 # Launch multiple instances
-p_qty=$1
-if [ -z "$p_qty" ]; then
+
+instance_qty=$1
+image_name=$2
+
+if [ -z "$instance_qty" ]; then
   echo "Launches N quantity of XYZ instances."
   echo "Usage:  <this script> <qty of instances>"
   exit 1
 fi
+if [ -z "$image_name" ]; then
+    image_name="xenial"
+fi
 
 net_id="$(openstack network list | awk '/private/ {print $2}')"
-image_name="xenial-lxd"
-for m in $(seq 1 $p_qty); do
+for m in $(seq 1 $instance_qty); do
   server_name="${image_name}$(date +'%H%M%S')"
   echo ${server_name} ${p_name} ${net_id}
   openstack server create --image $image_name --flavor m1.test --key-name mykey --nic net-id=${net_id} $server_name
